00cd464de289c29a2c171c9dfe838adadf5c645d
Abhishek Pandey <abhie9336@gmail.com>
2024-01-22 19:05:29 +0530

    completed

--- assignments/week1/day1/problem4.java
+++ assignments/week1/day1/problem4.java
@@ -12,3 +12,39 @@ Note that there is a space after https://, hence the URL is invalid.
 
 
 */
+
+import java.util.Scanner;
+
+class Solution{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String url = scn.nextLine();
+        System.out.println(isValidURL(url));
+       
+    }
+
+    public static boolean isValidURL(String url){
+         String[] tokens = url.split("//");
+         if(tokens.length <= 2) {
+            return false;
+         }
+        String protocol = tokens[0];
+        String webAddress = tokens[1];
+        System.out.println(webAddress);
+
+        if(!protocol.equals("https:") && !protocol.equals("http:")) {
+            return false;
+         }
+        else{
+            String[] webToken = webAddress.split("\\.");
+
+            if(webToken.length<2) return false;
+
+            if(webToken[0].charAt(0) == ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
